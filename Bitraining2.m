1868679e70ec36bda88a2ad4246641a3daadc708
Shao-Han Chen <Shao-Han.Chen@u.northwestern.edu>
2016-01-26 15:20:49 -0600

    add

--- Bitraining2.m
+++ Bitraining2.m
@@ -9,8 +9,8 @@ w1 = 1;
 w2 = 1;
 n0 = 10^(-2);    %noise variance
 
-iternums = 1:10; % number of iterations
-N_realization = 500; % Number of times to run simulation
+iternums = 1:5; % number of iterations
+N_realization = 100; % Number of times to run simulation
 
 C1 = zeros(N_realization, length(iternums));
 C2 = zeros(N_realization, length(iternums));
@@ -46,18 +46,16 @@ for realization_idx = 1 : N_realization
         x2_b = sign(randn(1,M));  
         
         %% bi-directional training
-            v11_old = v11;
-            v12_old = v12;
-            v21_old = v21;
-            v22_old = v22;
 
             N1 = sqrt(n0)*(randn(2,M)+1i*randn(2,M))/sqrt(2);
             N2 = sqrt(n0)*(randn(2,M)+1i*randn(2,M))/sqrt(2);
             
             %%Backward Training: sudo-LS Algorithm
-            [v11, v12] = S_LS_User1_Brutal(H11, H12, H21, H22, g1, g2, v21_old, v22_old, M, n0, N1, N2, x1_b, x2_b, w1, w2); 
-            [v21, v22] = S_LS_User2_Brutal(H11, H12, H21, H22, g1, g2, v11_old, v12_old, M, n0, N1, N2, x1_b, x2_b, w1, w2); 
+            for k1 = 1 : 20
+            [v11, v12] = S_LS_User1_Brutal(H11, H12, H21, H22, g1, g2, v21, v22, M, n0, N1, N2, x1_b, x2_b, w1, w2);
+            [v21, v22] = S_LS_User2_Brutal(H11, H12, H21, H22, g1, g2, v11, v12, M, n0, N1, N2, x1_b, x2_b, w1, w2); 
             %norm(v11)^2+norm(v12)^2
+            end
 
 
             %%Forward Training: LS Algorithm
@@ -90,4 +88,4 @@ p3=plot(iternums, mean(C1(:,:,30))+mean(C2(:,:,30)));
 
 
 axis([1 numiters 0 12])
-
+%}
